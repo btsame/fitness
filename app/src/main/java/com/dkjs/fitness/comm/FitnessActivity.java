7bd31e8b2f058d5148093891fcf76641c2d2acce
administrator <administrator@zhiqiudeMacBook.local>
2016-06-21 22:18:18 +0800

    init project

---
+++ app/src/main/java/com/dkjs/fitness/comm/FitnessActivity.java
@@ -0,0 +1,16 @@
+package com.dkjs.fitness.comm;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by administrator on 16/6/21.
+ */
+public class FitnessActivity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/dkjs/fitness/comm/FitnessApplication.java
@@ -0,0 +1,15 @@
+package com.dkjs.fitness.comm;
+
+import android.app.Application;
+
+/**
+ * Created by administrator on 16/6/21.
+ */
+public class FitnessApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
