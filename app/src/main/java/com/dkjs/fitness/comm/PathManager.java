9f214e0941090316bdef94c6d5e33028ad01f299
administrator <administrator@zhiqiudeMacBook.local>
2016-06-22 21:43:55 +0800

    init project

---
+++ app/src/main/java/com/dkjs/fitness/comm/PathManager.java
@@ -0,0 +1,14 @@
+package com.dkjs.fitness.comm;
+
+/**
+ * Created by administrator on 16/6/22.
+ */
+public class PathManager {
+
+    private static final String storageRootDir = "fitness";
+
+
+    static{
+
+    }
+}
