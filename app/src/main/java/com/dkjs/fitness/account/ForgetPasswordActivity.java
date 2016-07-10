2c45344a9d3534934497a0bc1bc3241bf51587bf
yangzhili <yangzhili756@163.com>
2016-07-10 15:15:35 +0800

    添加登录 注册 忘记密码 界面

---
+++ app/src/main/java/com/dkjs/fitness/account/ForgetPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.dkjs.fitness.account;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dkjs.fitness.R;
+import com.dkjs.fitness.comm.FitnessActivity;
+
+public class ForgetPasswordActivity extends FitnessActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
