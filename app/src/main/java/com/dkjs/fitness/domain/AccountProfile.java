2f27fd312c12c4f4ee758d36c47280452b528518
administrator <595883397@qq.com>
2016-07-10 15:29:43 +0800

    add domain profile

---
+++ app/src/main/java/com/dkjs/fitness/domain/AccountProfile.java
@@ -0,0 +1,18 @@
+package com.dkjs.fitness.domain;
+
+import com.maxleap.MLClassName;
+import com.maxleap.MLObject;
+
+/**
+ * Created by administrator on 16/7/9.
+ */
+
+@MLClassName("accountProfile")
+public class AccountProfile extends MLObject{
+
+    private int sex;
+
+
+
+
+}
