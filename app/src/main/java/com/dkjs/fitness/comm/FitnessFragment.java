df7bf21966918470637627bb13e22be18c8ebc45
yangzhili <yangzhili756@163.com>
2016-07-16 19:57:10 +0800

    我的-模块添加布局

--- app/src/main/java/com/dkjs/fitness/comm/FitnessFragment.java
+++ app/src/main/java/com/dkjs/fitness/comm/FitnessFragment.java
@@ -1,12 +1,17 @@
 package com.dkjs.fitness.comm;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dkjs.fitness.R;
 
 /**
  * Created by administrator on 16/7/10.
  */
 public class FitnessFragment extends Fragment{
-
-
 
 }
---
+++ app/src/main/java/com/dkjs/fitness/mine/UserInfoActivity.java
@@ -0,0 +1,22 @@
+package com.dkjs.fitness.mine;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.LayoutRes;
+
+import com.dkjs.fitness.R;
+import com.dkjs.fitness.comm.FitnessActivity;
+
+/**
+ * Created by Administrator on 2016/7/16.
+ */
+public class UserInfoActivity extends FitnessActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+
+}
