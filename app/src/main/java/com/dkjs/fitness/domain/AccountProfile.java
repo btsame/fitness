ba39d62afefdde6de3cb135b78f569295027d19c
administrator <595883397@qq.com>
2016-07-16 10:56:45 +0800

    1.add AccountProfile

--- app/src/main/java/com/dkjs/fitness/domain/AccountProfile.java
+++ app/src/main/java/com/dkjs/fitness/domain/AccountProfile.java
@@ -11,6 +11,8 @@ import com.maxleap.MLObject;
 public class AccountProfile extends MLObject{
 
     private int sex;
+    private int height;
+    private int weight;
 
     public int getSex() {
         return getInt("sex");
@@ -18,5 +20,21 @@ public class AccountProfile extends MLObject{
 
     public void setSex(int sex) {
         put("sex", sex);
+    }
+
+    public int getHeight() {
+        return getInt("height");
+    }
+
+    public void setHeight(int height) {
+        put("height", height);
+    }
+
+    public int getWeight() {
+        return getInt("weight");
+    }
+
+    public void setWeight(int weight) {
+        put("weight", weight);
     }
 }
