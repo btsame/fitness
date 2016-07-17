75d5c2267b65ac92fa92deb3b4970f8e26a9254e
yangzhili <yangzhili756@163.com>
2016-07-17 11:30:08 +0800

    添加编辑个人资料时间城市选择器

--- app/src/main/java/com/dkjs/fitness/mine/UserInfoActivity.java
+++ app/src/main/java/com/dkjs/fitness/mine/UserInfoActivity.java
@@ -3,20 +3,44 @@ package com.dkjs.fitness.mine;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.LayoutRes;
+import android.view.View;
+import android.widget.Toast;
 
 import com.dkjs.fitness.R;
+import com.dkjs.fitness.citydateselector.picker.DatePicker;
+import com.dkjs.fitness.citydateselector.task.AddressInitTask;
 import com.dkjs.fitness.comm.FitnessActivity;
+
+import java.util.Calendar;
 
 /**
  * Created by Administrator on 2016/7/16.
  */
 public class UserInfoActivity extends FitnessActivity {
 
-
+    private Calendar calendar = Calendar.getInstance();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
     }
 
+    public void setCity(View view) {
+        new AddressInitTask(this).execute("北京", "北京", "东城区");
+    }
+
+    public void setDate(View view) {
+        DatePicker picker = new DatePicker(this);
+        picker.setRange(1900, 2030);
+        picker.setSelectedItem(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DAY_OF_MONTH));
+        picker.setOnDatePickListener(new DatePicker.OnYearMonthDayPickListener() {
+            @Override
+            public void onDatePicked(String year, String month, String day) {
+//                showToast(year + "-" + month + "-" + day);
+
+                Toast.makeText(UserInfoActivity.this,year + "-" + month + "-" + day,Toast.LENGTH_LONG).show();
+            }
+        });
+        picker.show();
+    }
 }
---
+++ citydateselector/src/main/java/com/dkjs/fitness/citydateselector/AppConfig.java
@@ -0,0 +1,8 @@
+package com.dkjs.fitness.citydateselector;
+
+import com.dkjs.fitness.citydateselector.BuildConfig;
+
+public class AppConfig {
+    public static boolean DEBUG_ENABLE = BuildConfig.DEBUG;// 是否调试模式
+    public static String DEBUG_TAG = "fitness";// LogCat的标记
+}
