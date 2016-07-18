4a9b41de48a5c196bba3075eee34b3f1b388c7cf
yangzhili <yangzhili756@163.com>
2016-07-18 21:37:03 +0800

    增加我的 发起活动内容

---
+++ app/src/main/java/com/dkjs/fitness/mine/ActivityCreateActivity.java
@@ -0,0 +1,23 @@
+package com.dkjs.fitness.mine;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.dkjs.fitness.R;
+import com.dkjs.fitness.comm.FitnessActivity;
+
+/**
+ * Created by Administrator on 2016/7/18.
+ */
+public class ActivityCreateActivity extends FitnessActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+    }
+
+}
