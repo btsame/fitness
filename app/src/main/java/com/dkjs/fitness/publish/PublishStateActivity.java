a3f058f0d1452f874be9e18f30f553411c897540
bo yang <595883397@qq.com>
2016-07-21 18:37:18 +0800

    1、完善活动和状态功能；

---
+++ app/src/main/java/com/dkjs/fitness/publish/PublishStateActivity.java
@@ -0,0 +1,21 @@
+package com.dkjs.fitness.publish;
+
+import android.os.Bundle;
+
+import com.dkjs.fitness.R;
+import com.dkjs.fitness.comm.FitnessActivity;
+
+/**
+ * Created by Administrator on 2016/7/21.
+ */
+public class PublishStateActivity extends FitnessActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_publish_state);
+    }
+
+
+}
